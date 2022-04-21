534e2920e1940c25b01711701ecf826a3e786450
subminaldia <104077256+subminaldia@users.noreply.github.com>
2022-04-21 16:29:48 +0300

    Update MavenWrapperDownloader.java

--- MavenWrapperDownloader.java
+++ MavenWrapperDownloader.java
@@ -106,7 +106,6 @@ public class MavenWrapperDownloader {
             });
         }
         URL website = new URL(urlString);
-        ReadableByteChannel rbc;
         rbc = Channels.newChannel(website.openStream());
         FileOutputStream fos = new FileOutputStream(destination);
         fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
